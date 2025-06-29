b1f7007e5d5aff78ccdd2f342008d208cbc517ff
fledit-sh <44525727+fledit-sh@users.noreply.github.com>
2025-06-29 16:57:53 +0200

    Fix FensapRecipe to return FensapRunJob

--- glacium/recipes/fensap.py
+++ glacium/recipes/fensap.py
@@ -1,17 +1,17 @@
-"""Recipe integrating Pointwise mesh generation jobs."""
+"""Recipe containing jobs to run the FENSAP solver."""
 
 from glacium.managers.RecipeManager import RecipeManager, BaseRecipe
-from glacium.engines.fensap import FensapEngine
+from glacium.engines.fensap import FensapRunJob
 
 @RecipeManager.register
 class FensapRecipe(BaseRecipe):
-    """Run the Pointwise GCI and mesh generation scripts."""
+    """Run the FENSAP solver."""
 
     name = "fensap"
     description = "Run fensap scripts"
     def build(self, project):
         return [
-            FensapEngine(project),
+            FensapRunJob(project),
         ]
 
 
