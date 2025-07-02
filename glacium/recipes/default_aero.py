7aae61f5fc625210003ad3676bf265952c7c8bdf
pineOapple <noel@ernsting-luz.de>
2025-07-02 13:49:40 +0200

    FEAT
    multishot

--- glacium/recipes/default_aero.py
+++ glacium/recipes/default_aero.py
@@ -26,23 +26,7 @@ class DefaultAero(BaseRecipe):
             XfoilRefineJob(project),
             XfoilThickenTEJob(project),
             XfoilConvertJob(project),
-            XfoilBoundaryLayerJob(project),
-            XfoilPolarsJob(project),
-            XfoilSuctionCurveJob(project),
-        ]
-
-@RecipeManager.register
-class MinimalXfoil(BaseRecipe):
-    """Minimal variant containing only the refine and thicken jobs."""
-
-    name = "minimal_xfoil"
-    description = "Minimaler XFOIL-Workflow"
-
-    def build(self, project):
-        return [
-            XfoilRefineJob(project),
-            XfoilThickenTEJob(project),
-            XfoilConvertJob(project),
             PointwiseGCIJob(project),
             Fluent2FensapJob(project),
         ]
+
