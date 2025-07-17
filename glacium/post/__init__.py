8585a797e50b979c98b87b76165e4c78eb2d8b8b
fledit-sh <44525727+fledit-sh@users.noreply.github.com>
2025-07-17 11:38:42 +0200

    Add Fensap importers and tests

--- glacium/post/__init__.py
+++ glacium/post/__init__.py
@@ -1,3 +1,8 @@
 from .processor import PostProcessor
+from .importers import FensapSingleImporter, FensapMultiImporter
 
-__all__ = ["PostProcessor"]
+__all__ = [
+    "PostProcessor",
+    "FensapSingleImporter",
+    "FensapMultiImporter",
+]
