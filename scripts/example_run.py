94b24cd735f531b41eaf3e27bebc7bdff8dfea02
pineOapple <noel@ernsting-luz.de>
2025-07-21 09:08:02 +0200

    Patched scripts

--- scripts/example_run.py
+++ scripts/example_run.py
@@ -6,6 +6,7 @@ from glacium.api import Run
 
 def main():
     # create projects below ./runs in the current directory
+
     run = (
         Run("Project01")
         .name("X Grid")
@@ -20,10 +21,14 @@ def main():
         .set("CASE_MVD", 20)
         .set("CASE_LWC", 0.0052)
         .set("CASE_YPLUS", 0.3)
-        .set("PWS_REFINEMENT", 0.5)
+        .set("PWS_REFINEMENT", 8)
+
+        # Global settings
         .set("N_CPU", 12)
         .set("FSP_MAX_TIME_STEPS_PER_CYCLE", 700)
         .set("FSP_GUI_FENSAP_MAX_TIME_STEPS_PER_CYCLE", 700)
+
+        # Job definitions
         .add_job("XFOIL_REFINE")
         .add_job("XFOIL_THICKEN_TE")
         .add_job("XFOIL_PW_CONVERT")
@@ -31,6 +36,8 @@ def main():
         .add_job("FLUENT2FENSAP")
         .add_job("FENSAP_RUN")
         .add_job("FENSAP_CONVERGENCE_STATS")
+
+        .set("FSP_GUI_FENSAP_MAX_TIME_STEPS_PER_CYCLE", 120)
         .add_job("DROP3D_RUN")
         .add_job("DROP3D_CONVERGENCE_STATS")
         .add_job("ICE3D_RUN")
@@ -55,8 +62,19 @@ def main():
     # _.set("PWS_REFINEMENT", 4)
     # project_grid4 = _.create()
 
-    project_grid1.run()
 
+    # project_grid1.run("XFOIL_REFINE")
+    # project_grid1.run("XFOIL_THICKEN_TE")
+    # project_grid1.run("XFOIL_PW_CONVERT")
+    # project_grid1.run("POINTWISE_GCI")
+    # project_grid1.run("FLUENT2FENSAP")
+    #project_grid1.run("FENSAP_RUN")
+    #project_grid1.run("FENSAP_CONVERGENCE_STATS")
+    #project_grid1.run("DROP3D_RUN")
+    #project_grid1.run("DROP3D_CONVERGENCE_STATS")
+    #project_grid1.run("ICE3D_RUN")
+    # project_grid1.run("ICE3D_CONVERGENCE_STATS")
+    # project_grid1.run("POSTPROCESS_SINGLE_FENSAP")
 
 
 
