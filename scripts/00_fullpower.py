3514dd15d2b9ce294f1cfcffb8e8dc80b0f779a3
pineOapple <noel@ernsting-luz.de>
2025-08-14 08:37:24 +0200

    Update 00_fullpower.py

--- scripts/00_fullpower.py
+++ scripts/00_fullpower.py
@@ -20,7 +20,7 @@ SCRIPTS = [
 
 
 def main(study_name: str | None = None) -> None:
-    study_name = "C02_V20_T10_L0595"
+    study_name = "C02_V20_T10_L052"
 
     base_dir = Path(study_name)
     base_dir.mkdir(parents=True, exist_ok=True)
