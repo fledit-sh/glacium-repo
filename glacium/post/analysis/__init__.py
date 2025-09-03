088b70c79df7797f8a47393db088a8be467aea4b
fledit-sh <44525727+fledit-sh@users.noreply.github.com>
2025-09-03 12:58:17 +0200

    refactor mesh viewports

--- glacium/post/analysis/__init__.py
+++ glacium/post/analysis/__init__.py
@@ -1,6 +1,7 @@
 from .ice_thickness import read_wall_zone, process_wall_zone, plot_ice_thickness
 from .ice_contours import load_contours, plot_overlay, animate_growth
 from .mesh_screenshots import generate_wireframes
+from .mesh_viewports import fensap_mesh_plots
 
 __all__ = [
     "read_wall_zone",
@@ -10,4 +11,5 @@ __all__ = [
     "plot_overlay",
     "animate_growth",
     "generate_wireframes",
+    "fensap_mesh_plots",
 ]
