656d6ef36eef3f49e0622ad11634627738ce882c
pineOapple <noel@ernsting-luz.de>
2025-09-07 18:56:09 +0200

    Update 00_fullpower.py

--- scripts/00_fullpower.py
+++ scripts/00_fullpower.py
@@ -15,7 +15,7 @@ DEFAULT_STUDY_NAME = "NACA0008_C0400_V50_T2_L0591_PWS2_500"
 SCRIPTS = [
     # "01_full_power_creation.py",
     # "02_full_power_gci.py",
-    "05_multishot_creation.py",
+    # "05_multishot_creation.py",
     # "06_multishot_analysis.py",
     "07_aoa0_projects.py",
     "08_clean_sweep_creation.py",
