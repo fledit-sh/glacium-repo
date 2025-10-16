f341b92b3ebb4eb8ef053ba1232377f090edeca1
pineOapple <noel@ernsting-luz.de>
2025-10-16 20:53:59 +0200

    Update 00_fullpower.py

--- scripts/00_fullpower.py
+++ scripts/00_fullpower.py
@@ -10,16 +10,16 @@ from pathlib import Path
 import argparse
 import subprocess
 
-DEFAULT_STUDY_NAME = "NACA23012_C0450_V50_T2_L0591_PWS4_500"
+DEFAULT_STUDY_NAME = "NACA23012_C0450_V50_T4_L0591_PWS4_500"
 
 SCRIPTS = [
-    # "01_full_power_creation.py",
-    # "02_full_power_gci.py",
-    # "05_multishot_creation.py",
+    "01_full_power_creation.py",
+    "02_full_power_gci.py",
+    "05_multishot_creation.py",
     # "06_multishot_analysis.py",
-    # "07_aoa0_projects.py",
-    # "08_clean_sweep_creation.py",
-    # "09_clean_sweep_analysis.py",
+    "07_aoa0_projects.py",
+    "08_clean_sweep_creation.py",
+    "09_clean_sweep_analysis.py",
     "10_iced_sweep_creation.py",
     "11_iced_sweep_analysis.py",
     "12_polar_compare.py",
