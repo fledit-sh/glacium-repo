e94743ef8ba78cc5600b0b38c26893f453348033
fledit-sh <44525727+fledit-sh@users.noreply.github.com>
2026-01-20 09:12:13 +0100

    H5 handler basics are working

---
+++ glacium/file/h5/h5handler.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+import h5py
+import logging
+from abc import ABC, abstractmethod
+from typing import Any
+import numpy as np
+logging.basicConfig(level=logging.DEBUG)
+
+
+class H5Interface(ABC):
+    @abstractmethod
+    def group(self, key: str) -> "H5Interface": ...
+
+    @abstractmethod
+    def attr(self, key: str, value: Any) -> "H5Interface": ...
+
+    @abstractmethod
+    def feed(self, key: str, *, path: str) -> "H5Interface": ...
+
+
+class H5Node(H5Interface):
+    def __init__(self, g: h5py.Group):
+        self._g = g
+
+    def group(self, key: str) -> "H5Node":
+        return H5Node(self._g.require_group(key))
+
+    def attr(self, key: str, value: Any) -> "H5Node":
+        self._g.attrs[key] = value
+        return self
+
+    def feed(self, key: str, *, path: str) -> "H5Node":
+        data = np.fromfile(path, dtype=np.uint8)
+        ds = self._g.require_dataset(key, shape=data.shape, dtype=np.uint8, exact=False)
+        ds[...] = data
+        ds.attrs["source_path"] = path
+        return self
+
+
+class H5(H5Node):
+    def __init__(self, name: str, mode: str = "w"):
+        self.name = name
+        self.mode = mode
+        self.file: h5py.File | None = None
+        self._log = logging.getLogger("H5")
+        self._g = None  # will be set on enter
+
+    def __enter__(self) -> "H5":
+        self._log.info("enter %s", self.name)
+        self.file = h5py.File(self.name, self.mode)
+        self._g = self.file
+        return self
+
+    def __exit__(self, exc_type, exc, tb) -> bool:
+        self._log.info("exit %s", self.name)
+        try:
+            if self.file is not None:
+                self.file.close()
+        finally:
+            self.file = None
+            self._g = None
+        return False
+
+
+with H5("test.h5") as h5:
+    h5.group("groot")
