58859a18d8fdd8aa8f07fd4030e6772e39a298fd
fledit-sh <44525727+fledit-sh@users.noreply.github.com>
2026-02-13 19:51:43 +0100

    Major refactoring and package organization. Easy access and division into smaller subpackages.

--- glacium2/lineparser/line.py
+++ glacium2/lineparser/line.py
@@ -1,15 +1,13 @@
+from dataclasses import dataclass
+from typing import Tuple, Enum
 
+class LineType:
+    pass
 
 @dataclass
-class Line(ABC):
+class Line:
     raw: str
-    result: Any | None = None
 
-    def __post_init__(self):
-        self.result = self.process()
-
-    def process(self) -> Any:
-        raise NotImplementedError
-
-    def __str__(self) -> str:
-        return self.raw.rstrip("\n")
+class LineParser:
+    def __init__(self, line: Line):
+        self.raw = line.raw
